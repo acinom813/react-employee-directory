7193900fc51a71754f9072bfad9f51ce3ed71115
Monica Hill <monica.r.hill@gmail.com>
2021-04-22 16:35:22 -0400

    add <p> to header

--- employee-directory/src/components/header.js
+++ employee-directory/src/components/header.js
@@ -5,26 +5,7 @@ function Header(props) {
     return (
         <div class="jumbotron">
             <h1 class="display-4">Employee Directory</h1>
-        
-           
-           
-           
-           
-            <header className="App-header">
-        <h1 className="title">Employee Directory</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-        
+              <p>Search and sort from a list of current employees.</p>
         </div>
     )
 }
