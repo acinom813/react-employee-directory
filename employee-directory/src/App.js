951978517980c95eb4a31d488686f5ecaab90e63
Monica Hill <monica.r.hill@gmail.com>
2021-04-22 15:48:07 -0400

    create JS map component

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -2,16 +2,17 @@ import React from "react";
 import Header from "./components/header";
 import Navigation from "./components/navigation";
 import EmployeeCard from "./components/empolyee-card";
+//import EmployeeList from "./components/employee-list"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <Wrapper>
+        <>
            <Header />
            <Navigation />
            <EmployeeCard />
-        </Wrapper>
+        </>
       
     </div>
   );
---
+++ employee-directory/src/components/employee-list.js
@@ -0,0 +1,20 @@
+import React from "react";
+import EmployeeCard from "./empolyee-card";
+
+function EmployeeList ({database}) {
+    return (
+        
+       database.map(employee => (
+        <EmployeeCard
+            key={employee.id}
+            image={employee.image}
+            name={employee.name}
+            dept={employee.department}
+            email={employee.email}
+            phone={employee.phone}
+        />
+
+       ))
+    )
+}
+export default EmployeeList;
