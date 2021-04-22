f08ca498fea9e3eaefb0cac8f507d7e7a847a3b4
Monica Hill <monica.r.hill@gmail.com>
2021-04-22 14:50:17 -0400

    update imports and function in App.js

--- employee-directory/src/components/header.js
+++ employee-directory/src/components/header.js
@@ -5,6 +5,26 @@ function Header(props) {
     return (
         <div class="jumbotron">
             <h1 class="display-4">Employee Directory</h1>
+        
+           
+           
+           
+           
+            <header className="App-header">
+        <h1 className="title">Employee Directory</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+        
         </div>
     )
 }
