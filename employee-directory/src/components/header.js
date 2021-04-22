a622c05f48d07dd9077459c5db5d338ef07dc2f2
Monica Hill <monica.r.hill@gmail.com>
2021-04-22 12:59:50 -0400

    create component folders

---
+++ employee-directory/src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import 'bootstrap/4.0.0/css/bootstrap.min.css';
+
+function Header(props) { 
+    return (
+        <div class="jumbotron">
+            <h1 class="display-4">Employee Directory</h1>
+        </div>
+    )
+}
+
+export default Header;
+
